cea5fe33a193e086a7397f327f2aabf9c06533ac
Eren <e.basaran2004@gmail.com>
2024-09-01 12:51:35 +0200

    Feat: Added a register page

--- Frontend/ClipHug/src/app/app.routes.ts
+++ Frontend/ClipHug/src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {AppLayoutComponent} from "./app-layout/app-layout.component";
 import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
 
 export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
   },
   {
     path: '',
