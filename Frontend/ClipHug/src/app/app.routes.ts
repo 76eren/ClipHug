240424d9277b6f496cb978db3e4b832956fde956
Eren <e.basaran2004@gmail.com>
2024-09-01 12:44:22 +0200

    Feat: Created a login page

--- Frontend/ClipHug/src/app/app.routes.ts
+++ Frontend/ClipHug/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {AppLayoutComponent} from "./app-layout/app-layout.component";
+import {LoginComponent} from "./login/login.component";
 
 export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
   {
     path: '',
     component: AppLayoutComponent,
