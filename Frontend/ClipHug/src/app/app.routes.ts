d019fcb2f39b0d9b00b313e5ddb3b6f271856504
Eren <e.basaran2004@gmail.com>
2024-09-01 02:50:59 +0200

    Set up a basic Angular project

---
+++ Frontend/ClipHug/src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {AppLayoutComponent} from "./app-layout/app-layout.component";
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppLayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      }
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard' +
+      '',
+  },
+];
