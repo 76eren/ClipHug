dc051ca253ec768f56f6afd54c0171bf62f54610
Eren <e.basaran2004@gmail.com>
2024-09-04 17:24:47 +0200

    Fix: Fixed filtering

--- Frontend/ClipHug/src/app/dashboard/dashboard.component.ts
+++ Frontend/ClipHug/src/app/dashboard/dashboard.component.ts
@@ -1,105 +1,107 @@
-import {Component} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {LucideAngularModule} from "lucide-angular";
-import {FormsModule} from "@angular/forms";
-import {VideoService} from "../shared/service/requests/video.service";
-import {VideoModel} from "../shared/models/Login/video.model";
-import {ItemComponent} from "./item/item.component";
-import {ToastrService} from "ngx-toastr";
-import {ThumbnailsService} from "../shared/service/requests/thumbnails.service";
-
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LucideAngularModule } from 'lucide-angular';
+import { FormsModule } from '@angular/forms';
+import { VideoService } from '../shared/service/requests/video.service';
+import { VideoModel } from '../shared/models/Login/video.model';
+import { ItemComponent } from './item/item.component';
+import { ToastrService } from 'ngx-toastr';
+import { ThumbnailsService } from '../shared/service/requests/thumbnails.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [CommonModule, LucideAngularModule, FormsModule, ItemComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrls: ['./dashboard.component.scss']
 })
-
 export class DashboardComponent {
   public videos: VideoModel[] = [];
   public videosFiltered: VideoModel[] = [];
-
+  private videosOriginal: VideoModel[] = [];
   videoUrlInputBar: string = "";
-
-  // Videos will be loaded in bulks because the server can't handle all the videos at once
   bulkAmount: number = 6;
   bulks: number = 0;
   page = 1;
 
-  public images: string[] = []
-
-  constructor(private videoService: VideoService, private toastr: ToastrService, public thumbnailService: ThumbnailsService) {
-  }
+  constructor(
+    private videoService: VideoService,
+    private toastr: ToastrService,
+    public thumbnailService: ThumbnailsService
+  ) {}
 
   ngOnInit() {
-    this.videoService
-      .getAllVideosFromSelf()
-      .subscribe(response => {
-        this.videos = response.payload;
+    this.loadVideos();
+  }
 
-        this.bulks = Math.ceil(this.videos.length / this.bulkAmount)
+  loadVideos(searchTerm: string = '') {
+    let videoObservable = this.videoUrlInputBar.startsWith('@') ?
+      this.videoService.getAllVideosFromUser(this.videoUrlInputBar) :
+      this.videoService.getAllVideosFromSelf();
 
-        this.videosFiltered = this.videos.slice(0, this.bulkAmount);
-        this.changeThumbnails();
-      })
+    videoObservable.subscribe({
+      next: response => {
+        this.handleVideoResponse(response.payload);
+      },
+      error: () => {
+        this.toastr.error("User not found");
+      }
+    });
+  }
+
+  handleVideoResponse(videos: VideoModel[]) {
+    this.videos = videos;
+    this.videosOriginal = videos;
+    this.updatePagination();
   }
 
   onInputChange() {
-    if (this.videoUrlInputBar === "") {
-      this.videosFiltered = this.videos.slice(0, this.bulkAmount);
-      return;
+    if (this.videoUrlInputBar === '') {
+      this.loadVideos();
+    } else if (this.videoUrlInputBar.startsWith('@')) {
+      this.loadVideos(this.videoUrlInputBar);
+    } else {
+      this.filterVideos();
     }
+  }
 
-    if (this.videoUrlInputBar.startsWith("@")) {
-      this.videoService
-        .getAllVideosFromUser(this.videoUrlInputBar)
-        .subscribe({
-          next: (response) => {
-            this.videosFiltered = response.payload;
-          },
-          error: (err) => {
-            this.toastr.error("User not found");
-          }
-        });
+  filterVideos() {
+    this.videos = this.videosOriginal.filter(video =>
+      video.fileName.includes(this.videoUrlInputBar) || video.videoId.includes(this.videoUrlInputBar)
+    );
+    this.updatePagination();
+  }
 
-      return;
-    }
+  updatePagination() {
+    this.page = 1;
+    this.bulks = Math.ceil(this.videos.length / this.bulkAmount);
+    this.updateFilteredVideos();
+  }
 
-
-    this.videosFiltered = [];
-    for (let i of this.videos) {
-      if (i.fileName.includes(this.videoUrlInputBar) || i.videoId.includes(this.videoUrlInputBar)) {
-        this.videosFiltered.push(i);
-      }
-    }
+  updateFilteredVideos() {
+    const startIndex = (this.page - 1) * this.bulkAmount;
+    this.videosFiltered = this.videos.slice(startIndex, startIndex + this.bulkAmount);
+    this.changeThumbnails();
   }
 
   changeThumbnails() {
-    let idsToGet: string[] = this.videosFiltered.map((video) => video.videoId);
-    this.videoService.getMultipleThumbnailsUrl(idsToGet).subscribe({ next: (response) => {
+    let idsToGet = this.videosFiltered.map((video) => video.videoId);
+    this.videoService.getMultipleThumbnailsUrl(idsToGet).subscribe({
+      next: (response) => {
         this.thumbnailService.setThumbnails(response.payload);
-      }});
+      }
+    });
   }
 
   previousPage() {
-    if (this.page === 1) {
-      return;
-    }
+    if (this.page === 1) return;
     this.page--;
-    const startIndex = (this.page - 1) * this.bulkAmount;
-    this.videosFiltered = this.videos.slice(startIndex, startIndex + this.bulkAmount);
-    this.changeThumbnails();
+    this.updateFilteredVideos();
   }
 
   nextPage() {
-    if (this.page === this.bulks) {
-      return;
-    }
+    if (this.page === this.bulks) return;
     this.page++;
-    const startIndex = (this.page - 1) * this.bulkAmount;
-    this.videosFiltered = this.videos.slice(startIndex, startIndex + this.bulkAmount);
-    this.changeThumbnails();
+    this.updateFilteredVideos();
   }
 }
